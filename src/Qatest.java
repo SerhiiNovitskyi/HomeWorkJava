cdb2fdc6181abb496845269945f31ac369983878
snovitskiy <5863172392>
2018-11-12 19:24:34 +0200

    complete DZ#2

--- src/Qatest.java
+++ src/Qatest.java
@@ -1,51 +1,33 @@
-import java.util.Calendar;
 import java.util.Scanner;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 
 public class Qatest {
-
     public static void main(String args[]) {
+//        FirstPerson firstPerson = new FirstPerson("Serj", "Novitskiy");
+//        SecondPerson secondPerson = new SecondPerson();
+//        secondPerson.setFirstName("Sasha");
+//        secondPerson.setSecondName("Sizov");
+        ThirdClass thirdClass = new ThirdClass();
+        //thirdClass.printInfo(firstPerson,secondPerson);
+
         Scanner in = new Scanner(System.in);
-        int z = 0;
-        System.out.println("Введите X: ");
-        int x = in.nextInt(); //add days
-        System.out.println("Введите Y: ");
-        int y = in.nextInt(); //add hours
+        System.out.println("Enter First Person's data: ");
+        System.out.println("Enter First Name: ");
+        String firstPersonName = in.nextLine();
+        System.out.println("Enter First Surname: ");
+        String firstPersonSurname = in.nextLine();
 
-        System.out.println("Вычеслаем степени чисел:" );
-        method1(x,y,z);
-        System.out.println("Выводим дату и время с измениением дня и часов");
-        calculateDate(x,y);
-    }
-    public static void method1(int x, int y, int z) {
+        System.out.println("Enter Second Person's data: ");
+        System.out.println("Enter Second Name: ");
+        String secondPersonName = in.nextLine();
+        System.out.println("Enter Second Surname: ");
+        String secondPersonSurname = in.nextLine();
+        SecondPerson secondPerson = new SecondPerson();
+        secondPerson.setFirstName(secondPersonName);
+        secondPerson.setSecondName(secondPersonSurname);
+        thirdClass.printInfo(new FirstPerson(firstPersonName,firstPersonSurname), secondPerson);
 
-        System.out.println("X в степени Y равно " + Math.pow(x, y)); // x^y
-        System.out.println("X + Y равно " + (x + y));
-        System.out.println("\n");
-        for (int i = 0; i < y; i++) {
-            z += x;
-            System.out.println(z + " na shagy " + i);
-        }
-        System.out.println("\n");
 
     }
-    public static void calculateDate(int x, int y) {
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_MONTH,x);
-        cal.add(Calendar.HOUR_OF_DAY,y);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("E yyyy.MM.dd 'и время' hh:mm:ss a zzz");
-        System.out.println("Изменненая дата " + dateFormat.format( cal.getTime() ) );
-
-        System.out.println(cal.get(Calendar.DAY_OF_MONTH) + "." + cal.get(Calendar.MONTH) + "." + cal.get(Calendar.YEAR) + "-" + cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE));
-
-    }
-
-
-
-
-
 
 
 }
---
+++ src/ThirdClass.java
@@ -0,0 +1,5 @@
+public class ThirdClass {
+    public void printInfo(FirstPerson firstUser, SecondPerson secondUser) {
+        System.out.println(firstUser.firstName + " " + firstUser.secondName + " " + secondUser.getFirstName() + " " + secondUser.getSecondName());
+    }
+}
